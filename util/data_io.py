a0afea58af4a58c5a91115b02ca3762175df79b2
lxy <lxy0xabcdefg@qq.com>
2023-11-08 13:55:57 +0800

    Add data directory

--- util/data_io.py
+++ util/data_io.py
@@ -4,20 +4,20 @@ import traceback
 from data.data_dir import DATA_DIR
 from util.constants import DATE, OPEN, CLOSE, LOW, HIGH, TR
 
-DATA_FILE_NAME = DATA_DIR.joinpath('data.xlsx')
-OUTPUT_FILE_NAME = DATA_DIR.joinpath('output.xlsx')
+USE_COLUMNS = (DATE, OPEN, CLOSE, LOW, HIGH, TR)
+DATA_FILE = DATA_DIR.joinpath('data.xlsx')
+OUTPUT_FILE = DATA_DIR.joinpath('output.xlsx')
 
 DATE_TIME_FORMAT = 'YYYY-MM-DD'
 
 
 def load_data():
     # noinspection PyTypeChecker
-    return (pd.read_excel(DATA_FILE_NAME, usecols=(DATE, OPEN, CLOSE, LOW, HIGH, TR))
-            .dropna(axis=0, subset=(DATE,)))
+    return pd.read_excel(DATA_FILE, usecols=USE_COLUMNS).dropna(axis=0, subset=(DATE,))
 
 
 def save_data(output: pd.DataFrame):
-    with pd.ExcelWriter(OUTPUT_FILE_NAME, datetime_format=DATE_TIME_FORMAT) as writer:
+    with pd.ExcelWriter(OUTPUT_FILE, datetime_format=DATE_TIME_FORMAT) as writer:
         # https://github.com/pandas-dev/pandas/issues/44284
         try:
             writer._datetime_format = DATE_TIME_FORMAT
